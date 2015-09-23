d2a1ac2efedbf25a2eb62c520e3d4197742caede
Rannveigg13 <rannveigg13@ru.is>
2015-09-23 16:33:58 +0000

    Documentation for domains

--- src/main/java/is/ru/honn/rufan/domain/Country.java
+++ src/main/java/is/ru/honn/rufan/domain/Country.java
@@ -2,9 +2,8 @@ package is.ru.honn.rufan.domain;
 
 /**
  * @author Hrafnkell Baldursson
- * @author Rannveig Guðmundsdóttir
- * @date 22/9/2015.
- * @version 1.0
+ * @author Rannveig Gudmundsdottir
+ * @version 1.0 22/9/2015.
  */
 public class Country {
 
@@ -18,18 +17,38 @@ public class Country {
      */
     public int getCountryId() { return countryId; }
 
+    /**
+     * Getter for the name of the country
+     * @return This country's ID. Should never be null.
+     */
     public String getName() { return name; }
 
+    /**
+     * Getter for the abbreviation of the country.
+     * @return This country's abbreviation
+     */
     public String  getAbbreviation() { return abbreviation; }
 
+    /**
+     * Sets the country ID
+     * @param countryId This country's ID
+     */
     public void setCountryId(int countryId) {
         this.countryId = countryId;
     }
 
+    /**
+     * Sets the country's abbreviation
+     * @param abbreviation This country's abbreviation
+     */
     public void setAbbreviation(String abbreviation) {
         this.abbreviation = abbreviation;
     }
 
+    /**
+     * Sets the name of the country
+     * @param name This country's name
+     */
     public void setName(String name) {
         this.name = name;
     }
--- src/main/java/is/ru/honn/rufan/domain/Player.java
+++ src/main/java/is/ru/honn/rufan/domain/Player.java
@@ -9,9 +9,8 @@ import java.util.List;
 
 /**
  * @author Hrafnkell Baldursson
- * @author Rannveig Guðmundsdóttir
- * @date 22/9/2015.
- * @version 1.0
+ * @author Rannveig Gudmundsdottir
+ * @version 1.0 22/9/2015.
  */
 public class Player {
 
@@ -25,34 +24,118 @@ public class Player {
     protected Country nationality;
     protected List<Position> positions = new ArrayList<Position>();
 
+    /**
+     * Gets the player ID
+     * @return This player's ID
+     */
     public int getPlayerId() { return playerId; }
+
+    /**
+     * Gets the players first name
+     * @return This players first name
+     */
     public String getFirstName() { return firstName; }
+
+    /**
+     * Gets the players last name
+     * @return This players last name
+     */
     public String getLastName() { return lastName; }
+
+    /**
+     * Gets the players height
+     * @return This players height
+     */
     public int getHeight() { return height; }
+
+    /**
+     * Gets the players weight
+     * @return This players weight
+     */
     public int getWeight() { return weight; }
+
+    /**
+     * Gets the players birthdate
+     * @return This players birthdate
+     */
     public Date getBirthDate() { return birthDate; }
+
+    /**
+     * Gets the players nationality
+     * @return This players nationality
+     */
     public Country getNationality() { return nationality ; }
+
+    /**
+     * Gets the players team ID
+     * @return This players team ID
+     */
     public  int getTeamId() { return  teamId; }
+
+    /**
+     * Gets the players position
+     * @return This players position
+     */
     public List<Position> getPositions() { return positions; }
 
+    /**
+     * Sets the players ID
+     * @param playerId ID of the player
+     */
     public void setPlayerId(int playerId) { this.playerId = playerId; }
 
+    /**
+     * Sets the players first name
+     * @param firstName First name of the player
+     */
     public void setFirstName(String firstName) { this.firstName = firstName; }
 
+    /**
+     * Sets the players last name
+     * @param lastName Last name of the player
+     */
     public void setLastName(String lastName) { this.lastName = lastName; }
 
+    /**
+     * Sets the players height
+     * @param height Height of the player
+     */
     public void setHeight(int height) { this.height = height; }
 
+    /**
+     * Sets the players weight
+     * @param weight Weight of the player
+     */
     public void setWeight(int weight) { this.weight = weight; }
 
+    /**
+     * Sets the players birthdate
+     * @param birthDate Birthdate of the player
+     */
     public void setBirthDate(Date birthDate) { this.birthDate = birthDate; }
 
+    /**
+     * Sets the players nationality
+     * @param nationality Nationality of the player
+     */
     public void setNationality(Country nationality) { this.nationality = nationality; }
 
+    /**
+     * Sets the players team ID
+     * @param teamId ID of the players team
+     */
     public void setTeamId(int teamId) { this.teamId = teamId; }
 
+    /**
+     * Sets the players positions
+     * @param positions Positions of the player
+     */
     public void setPositions(List<Position> positions) { this.positions = positions; }
 
+    /**
+     * Adds a player's position to a list of positions
+     * @param position The player's position to be added
+     */
     public void addPosition(Position position) {
         if(position != null){
             positions.add(position);
