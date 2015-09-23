89477552bdf7ec87de0bd687537c2c66c01bc2ce
kotkarl <kotkarl@gmail.com>
2015-09-23 18:43:15 +0000

    hh

--- src/main/java/is/ru/honn/rufan/Observer/ObserverInterface.java
+++ src/main/java/is/ru/honn/rufan/Observer/ObserverInterface.java
@@ -5,7 +5,7 @@ package is.ru.honn.rufan.Observer;
  * @author Rannveig Gudmundsdottir
  * @version 1.0 22/9/2015.
  *
- * This inteface represents an observer which is subscribed to a subject.
+ * This interface represents an observer which is subscribed to a subject.
  * It gets notified when the subject's data it's subscribed to changes.
  */
 public interface ObserverInterface {
